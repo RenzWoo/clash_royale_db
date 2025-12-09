f40fdc952793fd060ebe904c00b063ca8acf052f
Renz <159403394+RenzWoo@users.noreply.github.com>
2025-12-09 11:25:24 +0800

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, create_engine
-
-postgres_url = "postgresql://postgres:admin@localhost:5432/clashroyale"
-sqlite_url = "sqlite:///CRdatabase.db"
-engine = create_engine(sqlite_url)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
